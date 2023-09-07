6992d411d9b5f26a71928cd604005649d95ffb70
mahirkabir92 <mahirkabir92@gmail.com>
2023-09-06 23:15:14 -0400

    Part_1

---
+++ mongoose-flights/models/flight.js
@@ -0,0 +1,28 @@
+// models/flight.js
+const mongoose = require('mongoose');
+
+const flightSchema = new mongoose.Schema({
+  airline: {
+    type: String,
+    enum: ['American', 'Southwest', 'United'], 
+    required: true,
+  },
+  airport: {
+    type: String,
+    enum: ['AUS', 'DFW', 'DEN', 'LAX', 'SAN'], 
+    default: 'DEN', 
+  },
+  flightNo: {
+    type: Number,
+    required: true,
+    min: 10,
+    max: 9999,
+  },
+  departs: {
+    type: Date,
+    required: true,
+    default: () => new Date(new Date().setFullYear(new Date().getFullYear() + 1)), // One year from now
+  },
+});
+
+module.exports = mongoose.model('Flight', flightSchema);
